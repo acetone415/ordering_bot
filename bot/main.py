d199c42398871f8a9953ff8b0083d2dbdbd0856c
acetone415 <acetone415@gmail.com>
2021-11-15 14:49:12 +0300

    Add process_order

--- bot/main.py
+++ bot/main.py
@@ -8,9 +8,17 @@ bot = Bot(config.TOKEN)
 dp = Dispatcher(bot)
 
 
+@dp.message_handler()
+async def send_greeting(msg: Message):
+    await bot.send_message(chat_id=msg.chat.id,
+                           text='Что вы хотите выбрать?',
+                           reply_markup=kb.KB_MAIN)
+
+
 @dp.callback_query_handler(lambda callback: callback.data == 'help')
 async def process_help(callback_query: CallbackQuery):
-    HELP_INFO = """Some help info"""
+    HELP_INFO = """Отправьте боту любое сообщение. Выберите нужную команду.
+    """
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.from_user.id, text=HELP_INFO)
 
@@ -22,9 +30,11 @@ async def process_show_tracklist(callback_query: CallbackQuery):
                            text=db.Song.show_tracklist())
 
 
-@dp.message_handler()
-async def send_greeting(msg: Message):
-    await msg.reply('Что вы хотите выбрать?', reply_markup=kb.KB_MAIN)
+@dp.callback_query_handler(lambda callback: callback.data == 'order')
+async def process_order(callback_query: CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id,
+                           text='Введите номер песни')
 
 
 if __name__ == '__main__':
